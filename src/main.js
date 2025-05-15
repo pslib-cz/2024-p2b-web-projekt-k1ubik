9a4cd88a6f66257bb92167a8803671cac9c598c1
Jakub :) <148348818+k1ubik@users.noreply.github.com>
2025-05-15 10:51:21 +0200

    15052025-1100

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ import './styles/normalize.css';
 import './styles/components.css';
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
+
+    var swiper = new Swiper('.swiper', {
+      slidesPerView: 3,
+      direction: getDirection(),
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        resize: function () {
+          swiper.changeDirection(getDirection());
+        },
+      },
+    });
